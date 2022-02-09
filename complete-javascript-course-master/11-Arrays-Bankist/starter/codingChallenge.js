51df621bfc6683f10200effbca5c313aecbe6789
Yiwei <yiwei@Yiweis-MacBook-Air.local>
2022-02-09 22:14:21 +1100

    finish array section

--- complete-javascript-course-master/11-Arrays-Bankist/starter/codingChallenge.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/codingChallenge.js
@@ -43,4 +43,136 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1);
 console.log(avg2);
+
+// TOPIC: Challenge 3
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(ages => (ages <= 2 ? ages * 2 : 16 + ages * 4))
+    .filter((ages, i, arr) => ages >= 18)
+    .reduce((acc, ages, i, arr) => acc + ages / arr.length, 0);
+// NOTE: Another way calculating average:
+// 2 3; (2 + 3) / 2 = 2.5; === 2 / 2 + 3 / 2 = 2.5;
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1);
+console.log(avg2);
+
+// TOPIC: Challenge 4
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+
+dogs.forEach(dog => {
+  const recommendedFood = dog.weight ** 0.75 * 28;
+  dog.recommendedFood = recommendedFood;
+});
+// NOTE: Jonas's code:
+// NOTE: The Math.trunc() function returns the integer part of a number by removing any fractional digits.
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+console.log(dogs);
+
+// 2.
+// const NumOwnerExist = dogs
+//   .map(dog => dog.owners)
+//   .findIndex(owner => owner.includes('Sarah'));
+// const recommendedPortion = dogs[NumOwnerExist].recommendedFood * 0.1;
+// if (
+//   dogs[NumOwnerExist].curFood >
+//   dogs[NumOwnerExist].recommendedFood + recommendedPortion
+// ) {
+//   console.log('Eating Too Much!');
+// } else if (
+//   dogs[NumOwnerExist].curFood <
+//   dogs[NumOwnerExist].recommendedFood - recommendedPortion
+// ) {
+//   console.log('Eating Too Little');
+// }
+
+// NOTE: Jonas's code:
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  }`
+);
+
+// 3.
+// console.log(dogs.flatMap(dog => dog.owners));
+
+// curFood > recommendedFood * 1.1 -- Eating too much
+// curFood < recommendedFood * 0.9 -- Eating too little
+
+const isEatingTooMuch = function (recommendedFood, curFood) {
+  if (curFood > recommendedFood * 1.1) return 'Eating too much';
+  else if (curFood < recommendedFood * 0.9) return 'Eating too little';
+  else return 'Eating ok';
+};
+
+const ownersEatingTooMuch = dogs
+  .filter(
+    dog =>
+      isEatingTooMuch(dog.recommendedFood, dog.curFood) === 'Eating too much'
+  )
+  .flatMap(dog => dog.owners);
+console.log(ownersEatingTooMuch);
+
+const ownersEatingTooLittle = dogs
+  .filter(
+    dog =>
+      isEatingTooMuch(dog.recommendedFood, dog.curFood) === 'Eating too little'
+  )
+  .flatMap(dog => dog.owners);
+console.log(ownersEatingTooLittle);
+
+// NOTE: Jonas's code:
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+// .flat();
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+// .flat();
+console.log(ownersEatTooLittle);
+
+// 4.
+console.log(`${ownersEatingTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatingTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// 5.
+console.log(dogs.some(dog => dog.recommendedFood === dog.curFood));
+
+// 6.
+const dogEatingOkExist = dogs.some(
+  dog => isEatingTooMuch(dog.recommendedFood, dog.curFood) === 'Eating ok'
+);
+console.log(dogEatingOkExist);
+
+// NOTE: Jonas's code:
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+console.log(dogs.some(checkEatingOkay));
+
+// 7.
+const dogsEatingOk = dogs.filter(
+  dog => isEatingTooMuch(dog.recommendedFood, dog.curFood) === 'Eating ok'
+);
+console.log(dogsEatingOk);
+
+// 8.
+console.log(dogs.slice().sort((a, b) => a.recommendedFood - b.recommendedFood));
+
+// NOTE: Jonas's code:
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
 */
--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -65,13 +65,15 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 // NOTE: Instead of working with global variables, start passing the data that function needs actually into that function.
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   // NOTE: InnerHTML is an built-in function that is a little bit similar to text content. The difference is that text content simply returns the text itself while HTML returns everything, including the HTML. So all the HTML tags will be included.
 
   containerMovements.innerHTML = '';
   // .textContent = 0
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -79,7 +81,7 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
     </div>
   `;
     // NOTE: The insertAdjacentHTML() method of the Element interface parses the specified text as HTML or XML and inserts the resulting nodes into the DOM tree at a specified position.
@@ -90,14 +92,32 @@ const displayMovements = function (movements) {
   });
 };
 
-displayMovements(account1.movements);
-
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
-calcDisplayBalance(account1.movements);
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
 
 // TOPIC: COMPUTING USERNAMES
 
@@ -114,6 +134,133 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+  // Display balance
+  calcDisplayBalance(acc);
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+// TOPIC: IMPLEMENTING LOGIN
+
+// Event handlers
+
+let currentAccount;
+
+// NOTE: When we click the button in a form element. So in HTML, the default behavior, when we click the Submit button, is for the page to reload.
+
+// NOTE: Another thing that's great about forms, is that whenever we have one of these fields here, input it, and when we then hit enter, then that will actually automatically trigger a click event on this button.
+
+btnLogin.addEventListener('click', function (e) {
+  // NOTE: When you are working with forms, preventing the reload is common to do.
+
+  // Prevent form from submitting
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and welcome message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+
+    // NOTE: We can use this blur function or method. So just call blur, and so that will make this field loses its focus.
+
+    inputLoginPin.blur();
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+// TOPIC: IMPLEMENTING TRANSFERS
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  // Clean out input
+
+  // NOTE: Essentially, textContent gives you a textual representation of what a node contains. Think of it as being everything between the opening and closing tags (i.e. <span>this text</span>); <input> elements however cannot have children (content model: nothing). The value that is associated with them can only be accessed via the value property.
+
+  inputTransferTo.value = inputTransferAmount.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    console.log(currentAccount);
+    // Add movement
+    currentAccount.movements.push(amount);
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+// TOPIC: THE FINDINDEX METHOD
+
+// NOTE: The the findIndex method works almost the same way as find. But as the name says, findIndex returns the index of the found element and not the element itself.
+
+// NOTE: The find and findIndex methods get access to also the current index, and the current entire array. the both the find and findIndex methods were added to JavaScript in ES6. And so they will not work in like super old browsers.
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    // NOTE: The big difference here is that with indexOf, we can only search for a value that is in the array. So, if the array contains the 23, then it's true, and if not, then it's false. But on the other hand, with findIndex, we can create a complex condition like this one, and of course, it doesn't have to be the equality operator here. It can be anything that returns true or false.
+
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+    //Hide UI
+    containerApp.style.opacity = 0;
+    //Clear
+    inputClosePin.value = inputCloseUsername.value = '';
+  }
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -349,4 +496,252 @@ const max = movements.reduce(function (acc, mov) {
 console.log(max);
 
 // NOTE: Reduce method is by far the most useful array method.
+
+// TOPIC: THE MAGIC OF CHAINING METHODS
+
+const eurToUsd = 1.1;
+console.log(movements);
+// Pipeline
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+
+// NOTE: First we should not overuse chaining, so we should try to optimize it because chaining tons of methods one after the other can cause a real performance issues if we have really huge arrays. So if we have a huge chain of methods, chained one after the other, we should try to compress all the functionality that they do into as little methods as possible.
+
+// NOTE: Second, it is a bad practice in JavaScript to chain methods that mutate the underlying original array. And an example of that is the splice method. You should not chain a method like the splice or the reverse method.
+
+// TOPIC: THE FIND METHOD
+
+// NOTE: We can use the Find method to retrieve one element of an array based on a condition.
+
+// NOTE: The Find method will actually not return a new array but it will only return the first element in the array that satisfies this condition.
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+// NOTE: There are two fundamental differences between find and filter method. First Filter returns all the elements that match the condition while the Find method only returns the first one. Second and even more important, the Filter method returns a new array while Find only returns the element itself and not an array.
+
+console.log(accounts);
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account); //{owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222, username: 'jd'}
+
+// const accountNew = accounts.filter(acc => acc.owner === 'Jessica Davis');
+// console.log(...accountNew);
+
+// TOPIC: SOME AND EVERY
+
+console.log(movements);
+
+// NOTE: We can use the includes method to test if an array includes a certain value. However, we can only really test for equality if any value in the array is exactly equal to -130.
+
+// Equality
+console.log(movements.includes(-130));
+
+// NOTE: We can specify a condition using some method.
+
+// Condition
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposits = movements.some(mov => mov > 1500);
+console.log(anyDeposits);
+
+// Every
+// NOTE: Every method: the every method is pretty similar to the some method. The difference between them is that every only returns true if all of the elements in the array satisfy the condition that we pass in.
+
+// NOTE: If every element passes the test in our callback function, only then the every method returns true
+
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+
+// NOTE: We could also write this function separately and then pass the function as a callback.
+
+const deposits = mov => mov > 0;
+console.log(movements.some(deposits));
+console.log(movements.every(deposits));
+console.log(movements.filter(deposits));
+
+// TOPIC: FLAT AND FLATMAP
+
+// NOTE: The flat method has no callback function, and it removes the nested arrays and flattens the array, which is why the method is called flat.
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+
+// NOTE: It now goes even into the second level of nesting and also takes the element out of depth array.
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap
+// NOTE: flatMap only goes one level deep and we cannot change it. So if you do need to go deeper than just one level, you still need to use the flat method.
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
+
+// TOPIC: SORTING ARRAYS
+
+// Strings
+// NOTE: Sort is actually mutated the original array. So we have to be very careful with this method.
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort()); // ['Adam', 'Jonas', 'Martha', 'Zach']
+console.log(owners); // ['Adam', 'Jonas', 'Martha', 'Zach']
+
+// Numbers
+// NOTE: The sort method does the sorting based on strings by default.
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (b > a) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
+
+// NOTE: If you have a mixed array, like with strings and numbers together, then this is not gonna work and I advise you to simply not to use the sort method in these cases anyway.
+
+// TOPIC: MORE WAYS OF CREATING AND FILLING ARRAYS
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// NOTE: The reason for that is this weird behavior of this Array() function which does it so that whenever we only pass in one argument, then it creates a new empty argument with that length.
+
+const x = new Array(7);
+console.log(x); // [empty × 7]
+
+// NOTE: So this is not really useful except for one thing, because there is one method that we can call on this empty array and that is the fill() method.
+
+// console.log(x.map(() => 5));
+
+x.fill(1); // [1, 1, 1, 1, 1, 1, 1]
+// x.fill(1, 3); // [empty × 3, 1, 1, 1, 1]
+x.fill(1, 3, 5); // [empty × 3, 1, 1, empty × 2]
+console.log(x);
+arr.fill(23, 4, 6);
+console.log(arr); // [1, 2, 3, 4, 23, 23, 7]
+
+// NOTE: How to create an array programmatically
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y); // [1, 1, 1, 1, 1, 1, 1]
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z); // [1, 2, 3, 4, 5, 6, 7]
+
+// Create an array with 100 random dice
+// NOTE: What Math.random will generate is a random number between 0 to 1 (excludes).
+
+const diceArray = Array.from(
+  { length: 100 },
+  () => Math.trunc(6 * Math.random()) + 1
+);
+console.log(diceArray);
+
+// NOTE: Things like Strings, Maps or Sets, they are all Iterables in JavaScript. Iterables can be converted to real arrays using Array.from().
+
+// NOTE: Using querySelectorAll(), returns something called a NodeList, which is something like an array, which contains all the selected elements. But it's not a real array, and so it doesn't have methods like map() or reduce(). So if we actually wanted to use a real array method like that on a NodeList, we would first need to convert the NodeList to an array. And for that Array.from() is perfect.
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')].map(
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI2);
+});
+
+// TOPIC: ARRAY METHODS PRACTICE
+
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+console.log(bankDepositSum);
+
+// 2.
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .filter(mov => mov >= 1000).length;
+
+  // NOTE: So we did count plus plus which then increased the value from zero to one. But the result of this expression here is still zero. And so zero was returned here to the next iteration. And therefore in the end we will always have zero. So we should use ++count.
+
+  // .reduce((count, cur) => (cur >= 1000 ? count++ : count), 0);
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+console.log(numDeposits1000);
+
+let a = 10;
+console.log(a++); // 10
+console.log(++a); // 12
+console.log(a); // 12
+
+// 3.
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+// NOTE: We can access a object property by both Object.propertyName and Object['propertyName'].
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(deposits, withdrawals);
+// {deposits: 25180, withdrawals: -7340}
+console.log(accounts);
+console.log(accounts.flat());
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word =>
+      exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(' ');
+  return capitalize(titleCase);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
 */
